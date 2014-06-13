05abf789cb6e7b01895a717e9c9e73e550cc8d55
igmingo <igmingo@gmail.com>
2014-06-13 16:56:45 +0200

    Hola

---
+++ JuegoCoches/src/Juego.java
@@ -0,0 +1,12 @@
+
+public class Juego {
+
+	public static void main(String[] args) {
+		Coche c1=new Coche();
+		
+		c1.color = "Rojo";
+		System.out.println("El coche es " + c1.color);
+		
+	}
+	
+}
