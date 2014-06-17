02c0cbc1e0f9a31f6109fd5dcc9eeca4c6fd3d95
igmingo <igmingo@gmail.com>
2014-06-17 18:24:25 +0200

    Update

--- CentroEducativo/src/com/igmingo/gui/Principal.java
+++ CentroEducativo/src/com/igmingo/gui/Principal.java
@@ -8,24 +8,42 @@ public class Principal {
 
 	public static void main(String[] args) {
 	
-        Persona[] lista;
-        lista = new Persona[4];
+//        Persona[] lista;
+//        lista = new Persona[4];
+
+		Persona[] listap = new Persona [4]; // es lo mismo que Persona listap[] = new Persona [4];
+        
+		//Persona es una superclase de Alumno y Profesor
+		listap[0] = new Profesor("18832030Z", "Luis", 42, "Java", 10);	
+		listap[1] = new Profesor("19782030J", "Marta", 38, "Informática avanzada", 6);	
+		listap[2] = new Alumno ("20434500A","Ignacio Mingo",32 , "Java", "Aula 4", 4.45);
+		listap[3] = new Alumno ("21424400F","Juan Lopez",28 , "C++", "Aula 4", 7);
 	
-		lista[0] = new Profesor("19782030Z", "Luis", 42, "Java", 10);	
-		lista[1] = new Profesor("19782030Z", "Marta", 38, "Informática", 6);	
-		lista[2] = new Alumno ("20434500A","Ignacio Mingo",32 , "Java", "Aula 4", 4.45);
-		lista[3] = new Alumno ("21424400A","Juan Lopez",28 , "Java", "Aula 4", 7);
-	
-		listado(lista);
+		listado(listap);
 		
 	}
 	
 	public static void listado(Persona[] lista) {
 		System.out.println("Hay " + lista.length + " personas en la lista.");
-		for (int i=0; i<lista.length; i++) {
-			Persona p;
-			p = lista[i];
-			p.imprimir();					
+		
+//		for (int i=0; i<lista.length; i++) {
+//			Persona p;
+//			p = lista[i];
+//			p.imprimir();					
+//		}
+		
+		for (int i = 0; i < lista.length; i++) {
+			if (lista[i] instanceof Profesor) {
+				System.out.print("Es un profesor.");
+				Profesor pro = (Profesor) lista [i];
+				System.out.println(lista[i].getNombre() + " con " + pro.getExperiencia() + " años de experiencia.");
+			} else {
+				if (lista[i] instanceof Alumno) {
+					System.out.print("Es un alumno.");
+					Alumno alu = (Alumno) lista [i];
+					System.out.println(lista[i].getNombre() + " con una nota de " + alu.getNota());
+				}
+			}
 		}
 	}
 }
